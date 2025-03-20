3bfaf341e3efc037dce07a5841b1748176035ad5
ineahe <ineahe@gmail.com>
2025-03-20 22:38:38 +0900

    🎉 feat: landing page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: 'index.html', // 기본 index.html
+        index: "index.html", // 기본 index.html
+        landing: "src/components/landing/landing.html",
+        profile: "src/components/profile/profile.html",
+        profile-edit: "src/components/profile-edit/profile-edit.html",
+        profile-edit-sub: "src/components/profile-edit-sub/profile-edit-sub.html",
       },
     },
   },
-  appType: 'mpa', // fallback 사용안함
+  appType: "mpa", // fallback 사용안함
   server: {
     // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
   },
