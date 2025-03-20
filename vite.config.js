f97afa4f28be3ec8914b09c45a6622f497ce0385
johyunsoo33 <johyunsoo369@naver.com>
2025-03-20 15:53:43 +0900

    🧪 test: mainpage-nav test 🎉 init: vite.config modify 📝 docs: icon add

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: 'index.html', // 기본 index.html
+        index: "index.html", // 기본 index.html
+        main_visual: "src/components/main-visual/main-visual.html",
+        main_card: "src/components/main-card/main-card.html",
+        footer: "src/components/footer/footer.html",
+        category_contnet_page:
+          "src/components/category-contnet-page/category-contnet-page.html",
+        main_test: "src/components/main-nav/main-nav.html",
       },
     },
   },
-  appType: 'mpa', // fallback 사용안함
+  appType: "mpa", // fallback 사용안함
   server: {
     // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
   },
